209e70ed7c0d153cbc567e69689428fd8f4ab6bc
Jayshil <jayshilpatel147@gmail.com>
2022-01-14 21:55:03 +0530

    Not need to ingest all visits simultaneously to GP model

--- gpcheops/gpcheops.py
+++ gpcheops/gpcheops.py
@@ -678,13 +678,7 @@ def multiple_visits(input_folders, plan_params, t14, out_path=os.getcwd(), GP='E
         # juliet best fit model
         model = results_full.lc.evaluate(instruments[i])
         # juliet best fit gp model
-        if not jointGP:
-            gp_model = results_full.lc.model[instruments[i]]['GP']
-        else:
-            inst1 = ''
-            for k in instruments:
-                inst1 = inst1 + '_' + k
-            gp_model = results_full.lc.model[inst1[1:]]['GP']
+        gp_model = results_full.lc.model[instruments[i]]['GP']
         # juliet best fit transit model and its errors
         transit_model = results_full.lc.model[instruments[i]]['deterministic']
         transit_model_err = results_full.lc.model[instruments[i]]['deterministic_errors']
